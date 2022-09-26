f65450507278bbac0df8a83335e6620fc0893585
gaston chiruchi <gastonnchiruchi@gmail.com>
2022-09-26 14:00:52 -0300

    nuevos cambios

--- src/componentes/CardWidget/CardWidget.js
+++ src/componentes/CardWidget/CardWidget.js
@@ -3,8 +3,7 @@ import cart from  './Assets/cart.svg';
 const CardWidget = () => {
     return(
         <div>
-            <img src={cart} alt="carrito"/>
-            2
+            <img src={cart} alt="carrito" width="30" height="30"/>
         </div>
     );
 }
--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -6,9 +6,8 @@ const Navbar = () => {
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
           <a class="navbar-brand" href="./public/index.html">
-            <img src={Logo} alt="Logo Gatito" width="30" height="30" class="d-inline-block align-text-top">
+            <img src={Logo} alt="Logo Gatito" width="30" height="30" class="d-inline-block align-text-top"/>
             Petshop
-            </img>
           </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
